80c087306c421ca2ab7290e54b8710d19832f87b
josiaspj <josiaspaesjr@gmail.com>
2015-01-16 14:40:39 -0300

    Layouts

--- HttpClient/app/src/main/java/com/android/josias/httpclient/AsyncTaskActivity.java
+++ HttpClient/app/src/main/java/com/android/josias/httpclient/AsyncTaskActivity.java
@@ -1,39 +1,56 @@
 package com.android.josias.httpclient;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import com.android.josias.httpclient.connection.async.Connection;
 
 
 public class AsyncTaskActivity extends ActionBarActivity {
+
+    private TextView tvResponse;
+
+    private RequestService request;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task);
+
+        tvResponse = (TextView)findViewById(R.id.tvResponse);
+
+        progress = new ProgressDialog(this);
+        progress.setMessage("Carregando ....");
+        progress.show();
+
+        request = new RequestService();
+        request.execute();
     }
 
+    private class RequestService extends AsyncTask<Void, Void, String>{
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_async_task, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //EXECUÇÃO DA CONEXÃO
+        @Override
+        protected String doInBackground(Void... params) {
+            String result = Connection.sendRequestGet(
+                    "http://www.eukip.com/aulas/feedeventos.html");
+            return result;
         }
 
-        return super.onOptionsItemSelected(item);
+        //TRATAMENTO DA RESPOSTA DA CONEXÃO
+        @Override
+        protected void onPostExecute(String result) {
+            progress.dismiss();
+            if (result != null) {
+                tvResponse.setText(result);
+            }else{
+                tvResponse.setText("Opa! Não foi em ... você tem conexão com a internet?");
+            }
+        }
     }
+
 }
--- HttpClient/app/src/main/java/com/android/josias/httpclient/VolleyActivity.java
+++ HttpClient/app/src/main/java/com/android/josias/httpclient/VolleyActivity.java
@@ -1,39 +1,46 @@
 package com.android.josias.httpclient;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import com.android.josias.httpclient.connection.volley.requests.ExampleRequest;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 
-public class VolleyActivity extends ActionBarActivity {
+public class VolleyActivity extends ActionBarActivity implements Response.Listener, Response.ErrorListener {
+
+    private TextView tvResponse;
+
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley);
-    }
 
+        tvResponse = (TextView)findViewById(R.id.tvResponse);
+
+        progress = new ProgressDialog(this);
+        progress.setMessage("Carregando ....");
+        progress.show();
+
+        ExampleRequest req = new ExampleRequest();
+        req.requestImageGet(VolleyActivity.this, VolleyActivity.this);
+
+    }
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progress.dismiss();
+        tvResponse.setText("Opa! Não foi em ... você tem conexão com a internet?");
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_volley, menu);
-        return true;
+    public void onResponse(Object response) {
+        progress.dismiss();
+        tvResponse.setText(response.toString());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
